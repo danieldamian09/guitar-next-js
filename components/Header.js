47039d6c6f0f6cee4af306e64213692fd1e421e6
danieldamian09 <humbertorivero9@gmail.com>
2022-02-17 21:10:53 -0300

    component of Image from next

--- components/Header.js
+++ components/Header.js
@@ -1,20 +1,21 @@
 import Link from "next/link";
-import styles from "../styles/Header.module.css"
+import Image from "next/image";
+import styles from "../styles/Header.module.css";
 
 const Header = () => {
 	return (
 		<header className={styles.header}>
 			<div className="contenedor">
-        <div>
-
-        </div>
-        <nav>
-          <Link href="/">Inicio</Link>
-          <Link href="/nosotros">Nosotros</Link>
-          <Link href="/blog">Blog</Link>
-          <Link href="/tienda">Tienda</Link>
-        </nav>
-      </div>
+				<Link href="/" passHref>
+					<Image src="/img/logo.svg" width={400} height={100} alt="logo" />
+				</Link>
+				<nav>
+					<Link href="/">Inicio</Link>
+					<Link href="/nosotros">Nosotros</Link>
+					<Link href="/blog">Blog</Link>
+					<Link href="/tienda">Tienda</Link>
+				</nav>
+			</div>
 		</header>
 	);
 };
