cfa43e775c68dd4fd58b77a1f1f4a023712229a3
danieldamian09 <humbertorivero9@gmail.com>
2022-02-21 15:35:17 -0300

    adding ServerSideProps and StaticProps to query the API

--- pages/blog.js
+++ pages/blog.js
@@ -1,13 +1,42 @@
 import Layout from "../components/Layout";
 
-const Blog = () => {
+const Blog = ({entradas}) => {
+
+	console.log(entradas)
+
 	return (
-		<Layout
-      pagina="blog"
-    >
+		<Layout pagina="blog">
 			<div>Desde Blog</div>
 		</Layout>
 	);
 };
+
+// Server Side Props
+// export async function getServerSideProps() {
+
+// 	const url = `http://localhost:1337/blogs`;
+// 	const respuesta = await fetch(url)
+// 	const entradas = await respuesta.json()
+
+// 	return {
+// 		props: {
+// 			entradas
+// 		}
+// 	}
+// }
+
+// Static Props
+export async function getStaticProps() {
+
+	const url = `http://localhost:1337/blogs`;
+	const respuesta = await fetch(url)
+	const entradas = await respuesta.json()
+
+	return {
+		props: {
+			entradas
+		}
+	}
+}
 
 export default Blog;
