11df99452093b3b949677cec5d1cad64f081c518
danieldamian09 <humbertorivero9@gmail.com>
2022-02-22 12:19:28 -0300

    bring in API data with Static Props and Static Paths

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,27 +1,39 @@
-
 const EntradaBlog = ({entrada}) => {
+	console.log(entrada);
 
-  console.log(entrada)
+	return <div>EntradaBlog</div>;
+};
 
-  return (
-    <div>EntradaBlog</div>
-  )
+// Para obtener las rutas estaticas y poder usar getStaticProps
+export async function getStaticPaths() {
+	const url = `http://localhost:1337/blogs`;
+	const respuesta = await fetch(url);
+	const entradas = await respuesta.json();
+
+	// Paths de las rutas iteramos todas la entradas para retornar los ID (cuando tenemos pocas rutas)
+	const paths = entradas.map((entrada) => ({
+		params: {
+			id: entrada.id.toString(),
+		},
+	}));
+
+	return {
+		paths,
+		fallback: false,
+	};
 }
 
-// Bring in API Data
-export async function getServerSideProps({query: {id}}) {
-
-  console.log(id)
-
+// Para obtener toda la informacion que va a colocar en las vistas
+export async function getStaticProps({params: {id}}) {
 	const url = `http://localhost:1337/blogs/${id}`;
-	const respuesta = await fetch(url)
-	const entrada = await respuesta.json()
+	const respuesta = await fetch(url);
+	const entrada = await respuesta.json();
 
 	return {
 		props: {
-			entrada
-		}
-	}
+			entrada,
+		},
+	};
 }
 
-export default EntradaBlog
+export default EntradaBlog;
