a5f2b2a7feffdf6d0eb8511486a6e6fc0332a6ec
danieldamian09 <humbertorivero9@gmail.com>
2022-02-23 15:46:15 -0300

    query the API to get the guitars using getServerSideProps

--- pages/tienda.js
+++ pages/tienda.js
@@ -1,13 +1,28 @@
 import Layout from "../components/Layout";
 
-const Tienda = () => {
+const Tienda = ({guitarras}) => {
+
+	console.log(guitarras)
+
 	return (
-		<Layout
-      pagina="tienda Virtual"
-    >
-			<div>Desde Tienda</div>
+		<Layout pagina="tienda Virtual">
+			<main className="contenedor">
+				<h1 className="heading">Nuestra Colección</h1>
+			</main>
 		</Layout>
 	);
 };
+
+export async function getServerSideProps(){
+	const url = `${process.env.API_URL}/guitarras`;
+	const respuesta = await fetch(url)
+	const guitarras = await respuesta.json()
+
+	return {
+		props: {
+			guitarras
+		}
+	}
+}
 
 export default Tienda;
