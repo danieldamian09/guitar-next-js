773913720a8923b4e0af4c338aea9de03ed95fe4
danieldamian09 <humbertorivero9@gmail.com>
2022-03-10 19:11:08 -0300

    adding form the detail of the guitars

--- pages/guitarras/[url].js
+++ pages/guitarras/[url].js
@@ -19,6 +19,23 @@ const Producto = ({guitarra}) => {
 					<h3>{nombre}</h3>
 					<p className={styles.descripcion}>{descripcion}</p>
 					<p className={styles.precio}>{`$${precio}`}</p>
+					<form className={styles.formulario}>
+						<label>Cantidad:</label>
+						<select>
+							<option value="">-- Seleccione --</option>
+							<option value="1">1</option>
+							<option value="2">2</option>
+							<option value="3">3</option>
+							<option value="4">4</option>
+							<option value="5">5</option>
+							<option value="6">6</option>
+							<option value="7">7</option>
+						</select>
+						<input 
+							type="submit"
+							value="Agregar al Carrito"
+						/>
+					</form>
 				</div>
 			</div>
 		</Layout>
@@ -26,7 +43,6 @@ const Producto = ({guitarra}) => {
 };
 
 export async function getServerSideProps({query: {url}}) {
-	// utilizamos un filtro para poder tarer las guitarras, ya que por principios de REST API se viene por ID
 	const urlGuitarra = `${process.env.API_URL}/guitarras?url=${url}`;
 	const respuesta = await fetch(urlGuitarra);
 	const guitarra = await respuesta.json();
