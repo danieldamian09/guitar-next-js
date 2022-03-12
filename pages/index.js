27389c7cf2ba2b83089930bdc2c7374416363a41
danieldamian09 <humbertorivero9@gmail.com>
2022-03-12 14:50:34 -0300

    checking guitars on the home page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,29 @@
 import Layout from "../components/Layout";
+import Listado from "../components/Listado"
 
-export default function Home() {
+export default function Home({guitarras}) {
 	return (
 		<Layout
       pagina="Inicio"
     >
-			<h1 className="heading">Desde Inicio</h1>
+		<main className="contenedor">
+			<h1 className="heading">Nuestra Colección</h1>
+			<Listado 
+				guitarras={guitarras}
+			/>
+		</main>
 		</Layout>
 	);
+}
+
+export async function getServerSideProps(){
+	const url = `${process.env.API_URL}/guitarras`;
+	const respuesta = await fetch(url)
+	const guitarras = await respuesta.json()
+
+	return {
+		props: {
+			guitarras
+		}
+	}
 }
